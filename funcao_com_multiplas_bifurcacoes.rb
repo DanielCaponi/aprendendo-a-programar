7cc7809ea38e4750969a8e888abaa5127a7f7410
Lucas Caponi da Silva <danielrockmarolinha@gmail.com>
2023-02-07 00:01:53 -0300

    Aprendendo elsif

---
+++ funcao_com_multiplas_bifurcacoes.rb
@@ -0,0 +1,11 @@
+def funcao_com_multiplas_bifurcacoes(x)
+  retorno = ""
+  if x > 2 
+    retorno = "maior que dois"
+  elsif x == 2
+    retorno = "igual a dois"
+  else 
+    retorno = "menor que dois"
+  end
+  return retorno  
+end
